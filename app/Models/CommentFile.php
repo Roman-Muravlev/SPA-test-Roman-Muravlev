417c3eed0249ef70121f0bfcd3dd2909b41997ae
“Roman_Muravlev” <Boosterdummer@gmail.com>
2023-09-06 12:52:25 +0300

    Add migrations and models

---
+++ app/Models/CommentFile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CommentFile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'comment_id',
+        'file',
+    ];
+
+    public function comment(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class, 'id', 'comment_id');
+    }
+}
