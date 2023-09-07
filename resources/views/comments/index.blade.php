bceaf1a6ad793a2c8c45827198407ecc7bd63936
“Roman_Muravlev” <Boosterdummer@gmail.com>
2023-09-07 12:55:28 +0300

    Form for creating comment

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -5,13 +5,33 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <title>SPA</title>
 </head>
 <body>
-    <h1>Культяпка "!!!!!!</h1>
     <form action="{{ route('comments.store') }}" method="POST">
-          @csrf
-        <input type="submit" placeholder="send">
+        @csrf
+        <div class="form-group">
+            <input type="text" class="form-control m-3" name="name" placeholder="Enter your name">
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control m-3" name="email" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control m-3" name="url" placeholder="url">
+        </div>
+        <div class="form-group">
+            <textarea class="form-control m-3" name="text" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+            <input type="file" class="form-control-file m-3">
+        </div>
+        <div class="form-group">
+            <input type="hidden" class="form-control-hidden m-3" name="parent_id">
+        </div>
+        <button type="submit" class="btn btn-primary m-3">Submit</button>
     </form>
 </body>
 </html>
