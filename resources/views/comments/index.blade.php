1f83195738bb05bda6168a52abba72e6feaee558
“Roman_Muravlev” <Boosterdummer@gmail.com>
2023-09-19 16:47:45 +0300

    Replace textarea on CKeditor in index.blade

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -37,7 +37,7 @@ Add comment
                         <input type="text" class="form-control " name="url" placeholder="url">
                     </div>
                     <div class="form-group m-3">
-                        <textarea class="form-control " name="text" rows="3"></textarea>
+                        <textarea class="ckeditor form-control"  name="text" rows="3"></textarea>
                     </div>
                     <div class="form-group m-3">
                         <input type="file" name="file" class="form-control-file ">
@@ -78,6 +78,14 @@ Add comment
     <div class="m-3">
         {{ $comments->links() }}
     </div>
+
+<script src="//cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.ckeditor').ckeditor();
+    });
+</script>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
